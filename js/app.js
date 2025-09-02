5520aca15d4bf02c6b3cd7fc8174e84d7f2035ca
entieerramesofia <aperezjo@centro.edu.mx>
2025-09-02 11:48:46 -0600

    arregle_mensaje_ventana_agregue_ir_a_otra_pagina

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//alert('hola mundo');
+
+let toggleBtn = document.querySelector('#mode-toggle-btn');
+toggleBtn.addEventListener('click', function() {
+    alert('me diste click');
+
+document.body.classList.toggle('dark-mode');
+
+});
